5c0d17e1d5fc98660917554d78d341cf543bc4ee
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-08-29 09:45:31 -0300

    create order item and remove ;

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -6,19 +6,19 @@ const Model = use('Model')
 class Order extends Model {
 
     items(){
-        return this.hasMany('App/Models/OrderItem');
+        return this.hasMany('App/Models/OrderItem')
     }
 
     coupons(){
-        return this.belongsToMany('App/Models/Coupon');
+        return this.belongsToMany('App/Models/Coupon')
     }
 
     discounts(){
-        return this.hasMany('App/Models/Discount');
+        return this.hasMany('App/Models/Discount')
     }
 
     user(){
-        return this.belongsTo('App/Models/User', 'user_id', 'id');
+        return this.belongsTo('App/Models/User', 'user_id', 'id')
     }
 
 }
