409c345aa8361e5b039923cefd6e82f4fe5e3e87
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-21 20:14:36 -0300

    create controllers and initialize AuthController

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
+
+class AuthController {
+
+  async register({ request, response }) {
+    const trx = await Database.beginTransaction()
+
+    try {
+
+      const { name, surname, email, password } = request.all()
+
+      const data = {
+      	name,
+        surname,
+        email,
+        password
+      }
+
+      const user = await User.create(data, trx)
+      const userRole = await Role.findBy('slug', 'client')
+      await user.roles().attach([ userRole.id ], null, trx)
+
+      await trx.commit()
+      return response.status(201).send({ data: user })
+
+    } catch(error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Erro ao realizar cadastro!'
+      })
+    }
+  }
+
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+
+    let data = await auth.withRefreshToken().attempt(email, password)
+
+    return response.send({ data })
+  }
+
+  async refresh({ request, response, auth }) {
+    var refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const user = await auth.newRefreshToken().generate(refresh_token)
+
+    return response.send({
+      data: user
+    })
+  }
+
+  async logout({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const loggedOut = await auth
+      .authenticator('jwt')
+      .revokeTokens([refresh_token], true)
+
+    return response.status(204).send({})
+  }
+
+  async forgot({ request, response }) {
+    //
+  }
+
+  async remember({ request, response }) {
+    //
+  }
+
+  async reset({ request, response }) {
+    //
+  }
+
+}
+
+module.exports = AuthController
