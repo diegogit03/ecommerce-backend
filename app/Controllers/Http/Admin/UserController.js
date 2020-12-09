d73b5a4b3372f42476d53b22ea02a1c7b03b7b40
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-09 11:08:43 -0300

    index UserController

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -3,7 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const User = use('App/Models/User')
 /**
  * Resourceful controller for interacting with users
  */
@@ -17,7 +17,18 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = User.query()
+
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('surname', 'LIKE', `%${name}%`)
+      query.orWhere('email', 'LIKE', `%${name}%`)
+    }
+
+    const users = await query.paginate(pagination.page, pagination.limit)
+    return response.send(users)
   }
 
   /**
