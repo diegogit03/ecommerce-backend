6c0ddf3512e2a109d66b07b61c6d03a0623ef516
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-06-29 21:11:45 -0300

    create str_random helper

--- app/Models/PasswordReset.js
+++ app/Models/PasswordReset.js
@@ -2,24 +2,25 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const { str_random } = use('App/Helpers')
 
 class PasswordReset extends Model {
     static boot(){
 
-        super.boot()
+      super.boot()
 
-        this.addHook('beforeCreate', async model => {
-            model.token = await str_random(25)
-            const expires_at = new Date()
-            expires_at.setMinutes(expires_at.getMinutes() + 30)
-            model.expires_at = expires_at
-        })
+      this.addHook('beforeCreate', async model => {
+        model.token = await str_random(25)
+        const expires_at = new Date()
+        expires_at.setMinutes(expires_at.getMinutes() + 30)
+        model.expires_at = expires_at
+      })
 
     }
 
     // formata os valores para o padrão do mysql
     static get dates(){
-        return ['created_at', 'updated_at', 'expires_at']
+      return ['created_at', 'updated_at', 'expires_at']
     }
 }
 
