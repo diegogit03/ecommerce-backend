75899746a9e0aaddcb536035f5e3fefd1b2226f7
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-06-29 21:26:57 -0300

    image_single_upload helper

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -26,6 +26,26 @@ const str_random = async (length = 40) => {
   return string
 }
 
+/**
+ * Move um unico arquivo para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ * @param { FileJar } file - o arquivo a ser gerenciado
+ * @param { string } path - o caminho para onde o arquivo dever ser movido
+ */
+const image_single_upload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+  // gera um nome aleatorio
+  const random_name = await str_random(30)
+  let filename = `${new Date().getTime()}-${randomName}.${file.subtype}`
+
+  // renomeia o arquivo e move ele para o path
+  await file.move(path, {
+    name: filename
+  })
+
+  return file
+}
+
 module.exports = {
   str_random
 }
