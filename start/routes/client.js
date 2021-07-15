efe9ce84d059e0653b42b3752ccfd0bdc86f417b
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-07-15 18:04:48 -0300

    fix route controller

--- start/routes/client.js
+++ start/routes/client.js
@@ -16,7 +16,7 @@ Route.group(() => {
   Route.get('orders', 'OrderController.index')
   Route.get('order/:id', 'OrderController.show')
   Route.post('orders', 'OrderController.store')
-  Route.put('orders/:id', 'OrderController.put')
+  Route.put('orders/:id', 'OrderController.update')
 })
   .prefix('v1')
   .namespace('Client')
