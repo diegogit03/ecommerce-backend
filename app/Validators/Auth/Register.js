a4d146321fcc66c8dbef14511a3f11169003b983
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-10-21 20:24:03 -0300

    Custom messages in RegisterValidator

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -9,6 +9,18 @@ class Register {
       password: 'required|confirmed'
     }
   }
+
+  get messages () {
+    return {
+      'name.required': 'O nome é obrigatório!',
+      'surname.required': 'O sobrenome é obrigatório!',
+      'email.required': 'O e-mail é obrigatório!',
+      'email.email': 'E-mail inválido!',
+      'email.unique': 'Este E-mail já existe!',
+      'password.required': 'A senha é obrigatória!',
+      'password.confirmed': 'As senhas não são iguais!'
+    }
+  }
 }
 
 module.exports = Register
